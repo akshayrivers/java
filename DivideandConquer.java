a6a261269feb01892ab5b5e4f9c732283492786a
vinod akshat <pumkininriver@gmail.com>
2023-12-21 22:42:26 +0530

    added merge sort for strings[]

--- DivideandConquer.java
+++ DivideandConquer.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DivideandConquer {
 public static void printarr(int arr[]){
     for(int i =0; i <arr.length;i++){
@@ -44,10 +46,83 @@ public static void merge(int arr[],int si ,int mid, int ei){
         arr[i]=temp[k];
     }
 }
+private static void mergeSort(String[] arr, int left, int right) {
+        if (left < right) {
+            // Find the middle point
+            int mid = (left + right) / 2;
+
+            // Recursively sort the first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Merge Function for Strings
+    private static void merge(String[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        String[] leftArray = Arrays.copyOfRange(arr, left, mid + 1);
+        String[] rightArray = Arrays.copyOfRange(arr, mid + 1, right + 1);
+
+        // Merge the temporary arrays
+
+        // Initial indices of the two subarrays
+        int i = 0, j = 0;
+
+        // Initial index of the merged subarray
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i].compareTo(rightArray[j]) <= 0) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of leftArray[], if any
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of rightArray[], if any
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Utility function to print an array of Strings
+    private static void printArray(String[] arr) {
+        for (String str : arr) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int arr[]={6,3,9,5,2,8,3,23,475,6,77,9344,46636,544,6,75434,4,6,7,4,754,3,3};
         mergesort(arr, 0, arr.length-1);
         printarr(arr);
-    
+        String[] arr1 = { "sun", "earth", "mars", "mercury" };
+
+        System.out.println("Original Array:");
+        printArray(arr1);
+
+        mergeSort(arr1, 0, arr1.length - 1);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr1);
     }
 }
