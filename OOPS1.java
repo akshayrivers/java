53e9d2f392cc281423f19cfaa179d9a4a8d49ff2
vinod akshat <pumkininriver@gmail.com>
2023-12-24 23:38:39 +0530

    adding the OOPS concepts #1

--- OOPS1.java
+++ OOPS1.java
@@ -1,5 +1,30 @@
 public class OOPS1{
+    static class pen {
+        String color;
+        int tip;
+        void setcolor(String newcolor){
+            color =newcolor;
+        }
+        void settip(int newtip){
+            tip=newtip;
+        }
+    }
+    static class BankAccount{
+        public String username ;
+        private String password;
+        public void setpassword(String pwd){
+            password=pwd;
+        }
+    }
     public static void main(String[] args){
-        System.out.println("adding this tto git hub ");
+        pen p1 =new pen();
+        p1.setcolor("blue");
+        System.out.println(p1.color);
+        BankAccount myAccount=new BankAccount();
+        myAccount.username="akshay";
+        myAccount.setpassword("oogabooga");
+        System.out.println(myAccount.username);
+
+        
     }
 }
