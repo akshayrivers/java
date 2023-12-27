c6bd24a3a9f88e83da669a95668dbacfe81b6e7c
vinod akshat <pumkininriver@gmail.com>
2023-12-27 21:57:00 +0530

    added the new backtracking code

---
+++ backtracking.java
@@ -0,0 +1,25 @@
+class backtracking{
+    public static void changearr(int arr[],int i,int val){
+        //base case
+        if(i==arr.length){
+            Printarr(arr);
+            return;
+        }
+        //recursion
+        arr[i]=val;
+        changearr(arr, i+1, val+1);
+        arr[i]=arr[i]-2;
+    }
+    public static void Printarr(int arr[]){
+        for( int i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,5,6,7};
+        int val=0;
+        int i=0;
+        changearr(arr, i, val);
+        
+    }
+}
