06f061644569d15cbd15e746d7187457978fccf0
Vinod Akshat <pumkininriver@gmail.com>
2024-02-17 02:17:20 +0530

    stack using linked list

--- Stack/stack.java
+++ Stack/stack.java
@@ -12,28 +12,66 @@ class stack {
      * 3.Linked List-> variable length
      * @param args
      */
-
-     //Stack using ArrayList
-
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
         static class stack0{
-            static ArrayList<Integer> list=new ArrayList<>();
-            public static boolean isEmpty(){
-                return list.size()==0;
-            }
-            //push
-            public static void push(int data){
-                list.add(data);
-            }
-            //pop
-            public static int pop(){
-                int top=list.get(list.size()-1);
-                list.remove(list.size()-1);
-                return top;
-            }
-            //peek
-            public static int peek(){
-                return list.get(list.size()-1);
-            }
+            //Stack using ArrayList
+            // static ArrayList<Integer> list=new ArrayList<>();
+            // public static boolean isEmpty(){
+            //     return list.size()==0;
+            // }
+            // //push
+            // public static void push(int data){
+            //     list.add(data);
+            // 
+            // //pop
+            // public static int pop(){
+            //     int top=list.get(list.size()-1);
+            //     list.remove(list.size()-1);
+            //     return top;
+            // }
+            // //peek
+            // public static int peek(){
+            //     return list.get(list.size()-1);
+            // }
+
+            //Stack using linkedlist
+                static Node head = null;
+                public static boolean isEmpty(){
+                    return head==null;
+                }
+                //push
+                public static void push(int data){
+                    Node newNode=new Node(data);
+                    if(isEmpty()){
+                        head=newNode;
+                        return;
+                    }
+                    newNode.next = head;
+                    head=newNode;
+                }
+                //pop
+                public static int pop(){
+                    if(isEmpty()){
+                        return-1;
+                    }
+                    int top=head.data;
+                    head=head.next;
+                    return top;
+                }
+                //peek
+                public static int peek(){
+                    if(isEmpty()){
+                        return -1;
+                    }
+                    return head.data;
+                }
         }
      
     public static void main(String[] args) {
@@ -41,6 +79,7 @@ class stack {
         s.push(1);
         s.push(2);
         s.push(3);
+        s.push(6);
         while(!s.isEmpty()){
             System.out.print(s.peek()+"  ");
             s.pop();
