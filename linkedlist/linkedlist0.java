e882e9ea83ebbda186fdce81cfbd0c7e4e40b22b
Vinod Akshat <pumkininriver@gmail.com>
2024-02-16 21:17:21 +0530

    added the merge sort function on linked list

--- linkedlist/linkedlist0.java
+++ linkedlist/linkedlist0.java
@@ -280,6 +280,61 @@ class linkedlist0{
         System.out.println("Cycle removed from the list.");
     }
 
+    /*Merge sort on a linked list TC->O(nlogn)
+      step1: find middle of ll
+      step2:sort left half and right half 
+      step3:merge
+    */
+    public static Node mergesort(Node head){
+        if(head==null||head.next==null){
+            return head;
+        }
+        Node mid=getmid(head);
+        //left & right 
+        Node righthead=mid.next;
+        mid.next=null;
+        Node newleft=mergesort(head);
+        Node newright=mergesort(righthead);
+        //merge
+        return merge(newleft,newright);
+
+    }
+    public static Node getmid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;//mid node
+    }
+    private static Node merge(Node head1, Node head2) {
+        Node mergedll = new Node(-1);
+        Node temp = mergedll;
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next; // Move temp to the next node
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next; // Move temp to the next node
+            }
+           
+        }
+        while (head1!=null) {
+            temp.next=head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+        while (head2!=null) {
+            temp.next=head2;
+            head2=head2.next;
+            temp=temp.next;
+        }
+        return mergedll.next;
+    }
     
     public static void main(String args[]){
         linkedlist0 ll=new linkedlist0();
@@ -287,13 +342,19 @@ class linkedlist0{
         head.next=new Node(2);
         head.next.next=new Node(3);
         head.next.next.next=new Node(5);
-        head.next.next.next.next=head.next.next;
-        System.out.println("Size of linked list = "+ size);
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+        addfirst(8);
+        addfirst(9);
+        addfirst(12);
+        addfirst(0);
         print();
-        System.out.println(size);
+        mergesort(head);
+        print();
+        // System.out.println("Size of linked list = "+ size);
+        // System.out.println(isCycle());
+        // removeCycle();
+        // System.out.println(isCycle());
+        // print();
+        // System.out.println(size);
        /**System.out.println( removeFirst());
         **System.out.println(removeLast());
         print();System.out.println(isPalindrome());
