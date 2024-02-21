d3cc8872b51222bffaf28144e5fe3b03caef7bd3
Vinod Akshat <pumkininriver@gmail.com>
2024-02-21 15:02:23 +0530

    reversing a stack

--- Stacks/stack.java
+++ Stacks/stack.java
@@ -23,76 +23,61 @@ class stack {
     }
         static class stack0{
             //Stack using ArrayList
-            // static ArrayList<Integer> list=new ArrayList<>();
-            // public static boolean isEmpty(){
-            //     return list.size()==0;
-            // }
-            // //push
-            // public static void push(int data){
-            //     list.add(data);
-            // 
-            // //pop
-            // public static int pop(){
-            //     int top=list.get(list.size()-1);
-            //     list.remove(list.size()-1);
-            //     return top;
-            // }
-            // //peek
-            // public static int peek(){
-            //     return list.get(list.size()-1);
-            // }
-                // static ArrayList<Integer> list=new ArrayList<>();
-                // public static boolean isEmpty(){
-                //     return list.size()==0;
-                // }
-                // //push
-                // public static void push(int data){
-                //     list.add(data);
-                // }
-                // //pop
-                // public static int pop(){
-                //     int top=list.get(list.size()-1);
-                //     list.remove(list.size()-1);
-                //     return top;
-                // }
-                // //peek
-                // public static int peek(){
-                //     return list.get(list.size()-1);
-                // }
-            // //Stack using linkedlist
-            //     static Node head = null;
-            //     public static boolean isEmpty(){
-            //         return head==null;
-            //     }
-            //     //push
-            //     public static void push(int data){
-            //         Node newNode=new Node(data);
-            //         if(isEmpty()){
-            //             head=newNode;
-            //             return;
-            //         }
-            //         newNode.next = head;
-            //         head=newNode;
-            //     }
-            //     //pop
-            //     public static int pop(){
-            //         if(isEmpty()){
-            //             return-1;
-            //         }
-            //         int top=head.data;
-            //         head=head.next;
-            //         return top;
-            //     }
-            //     //peek
-            //     public static int peek(){
-            //         if(isEmpty()){
-            //             return -1;
-            //         }
-            //         return head.data;
-            //     }
+            class SusingAL{
+            static ArrayList<Integer> list=new ArrayList<>();
+            public static boolean isEmpty(){
+                return list.size()==0;
+            }
+            //push
+            public static void push(int data){
+                list.add(data);}
+            
+            //pop
+            public static int pop(){
+                int top=list.get(list.size()-1);
+                list.remove(list.size()-1);
+                return top;
+            }
+            //peek
+            public static int peek(){
+                return list.get(list.size()-1);
+            }
+        }
+            class SusingLL{
+            //Stack using linkedlist
+                static Node head = null;
+                public static boolean isEmpty(){
+                    return head==null;
+                }
+                //push
+                public static void push(int data){
+                    Node newNode=new Node(data);
+                    if(isEmpty()){
+                        head=newNode;
+                        return;
+                    }
+                    newNode.next = head;
+                    head=newNode;
+                }
+                //pop
+                public static int pop(){
+                    if(isEmpty()){
+                        return-1;
+                    }
+                    int top=head.data;
+                    head=head.next;
+                    return top;
+                }
+                //peek
+                public static int peek(){
+                    if(isEmpty()){
+                        return -1;
+                    }
+                    return head.data;
+                }
          
         }
-    
+            }
         public static void PushAtBottom(Stack<Integer> s, int data ){
             if(s.empty()){
                 s.push(data);
@@ -102,7 +87,7 @@ class stack {
             PushAtBottom(s,data);
             s.push(top);
         }
-     
+    
         // reverse a string using stack 
         public static String  reverseString(String str ){
             Stack<Character> s= new Stack<>();
